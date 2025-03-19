1ae8ff960bc5d9bac3b04101900290acb2db18e8
Josef Caique <jcaique10@gmail.com>
2025-03-18 22:40:30 -0300

    Creating Position class

---
+++ src/boardgame/Position.java
@@ -0,0 +1,44 @@
+package boardgame;
+
+import java.util.Objects;
+
+public class Position {
+    private Integer row;
+    private Integer column;
+
+    public Position(Integer row, Integer column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setValues(Integer row, Integer column){
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return Objects.equals(row, position.row) && Objects.equals(column, position.column);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
+}
