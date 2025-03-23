db679da308c87a01b1d221b92c7af8927897e075
Josef Caique <jcaique10@gmail.com>
2025-03-22 23:48:20 -0300

    chess exception and git position

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,11 +1,16 @@
 package chess;
 
+import boardgame.Position;
+
 public class ChessPosition {
 
     private Character column;
     private Integer row;
 
     public ChessPosition(Character column, Integer row) {
+        if (column < 'a'  || column > 'h' || row < 1 || row > 8) {
+            throw new ChessException("Error instantiating ChessPosition");
+        }
         this.column = column;
         this.row = row;
     }
@@ -14,15 +19,21 @@ public class ChessPosition {
         return column;
     }
 
-    public void setColumn(Character column) {
-        this.column = column;
-    }
-
     public Integer getRow() {
         return row;
     }
 
-    public void setRow(Integer row) {
-        this.row = row;
+    protected Position toPosition() {
+        return new Position(8 - row, column - 'a');
     }
+
+    protected ChessPosition fromPosition(Position position){
+        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString(){
+        return ""+ column + row;
+    }
+
 }
