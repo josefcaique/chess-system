7319c2df9557befc765521a7a8389c01c96c0c75
Josef Caique <jcaique10@gmail.com>
2025-03-26 23:08:56 -0300

    adding new rules

--- src/application/UI.java
+++ src/application/UI.java
@@ -2,9 +2,12 @@ package application;
 
 import chess.ChessMatch;
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -12,11 +15,19 @@ public class UI {
     public static final String ANSI_WHITE = "\u001B[37m";
     public static final String ANSI_YELLOW = "\u001B[33m";
 
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        } catch (RuntimeException e) {
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
+        }
+
+}
+
     public static void printBoard(ChessPiece[][] pieces) {
-
-
-
-
         for (int i=0; i<pieces[0].length; i++){
             System.out.println();
             System.out.print((8-i) + " ");
