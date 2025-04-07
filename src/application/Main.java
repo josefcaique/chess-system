f2a26dc065935f6d2a4ee4009d488dc9d1cf9568
Josef Caique <jcaique10@gmail.com>
2025-04-06 22:59:22 -0300

    Implementing checkmate logic

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,7 +20,7 @@ public class Main {
         List<ChessPiece> captured = new ArrayList<>();
 
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
 
             try {
                 UI.clearScreen();
@@ -49,6 +49,7 @@ public class Main {
             }
 
         }
-
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
